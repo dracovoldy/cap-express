8d2b5cfd01950e8d1bd09843d87ccb46390391a3
Mallik <abhishek.mallik@capgemini.com>
2019-04-26 08:15:14 -0400

    pdf

--- routes/pdf.js
+++ routes/pdf.js
@@ -3,7 +3,7 @@ const pool = require('../data/config');
 const PDFDocument = require('../modules/pdf');
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.get('/:id', (req, res) => {
     const doc = new PDFDocument();
     // let filename = req.body.filename
     let filename = "test-pdf"
@@ -14,36 +14,52 @@ router.post('/', (req, res) => {
     res.setHeader('Content-disposition', 'attachment; filename="' + filename + '"')
     res.setHeader('Content-type', 'application/pdf')
 
-    //content
-    const table0 = {
-        headers: ['Word', 'Comment', 'Summary'],
-        rows: [
-            ['Apple', 'Not this one', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla viverra at ligula gravida ultrices. Fusce vitae pulvinar magna.'],
-            ['Tire', 'Smells like funny', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla viverra at ligula gravida ultrices. Fusce vitae pulvinar magna.']
-        ]
-    };
+    const estimateId = req.params.id;
 
-    doc.table(table0, {
-        prepareHeader: () => doc.font('Helvetica-Bold'),
-        prepareRow: (row, i) => doc.font('Helvetica').fontSize(12)
+    pool.query('SELECT * FROM cust_estimates as i WHERE i.entry_id = ?', [estimateId], (error, result) => {
+        if (error) {
+            throw error;
+        } else {
+            //content
+            const table0 = {
+                headers: ['Client Fields', 'Data'],
+                rows: [
+                    ['Company', result.comp_name],
+                    ['Sector/Market unit', 'Smells like funny'],
+                    ['Primary region', 'Where are you'],
+                    ['Client name(s)', result.client_name],
+                    ['Title', 'SomeTitle'],
+                    ['Email', result.client_contact]
+                ]
+            };
+
+            doc.table(table0, {
+                prepareHeader: () => doc.font('Helvetica-Bold'),
+                prepareRow: (row, i) => doc.font('Helvetica').fontSize(12)
+            });
+
+            const table1 = {
+                headers: ['Name', 'Email', 'Phone'],
+                rows: [
+                    [result.cap_name, result.cap_email, result.cap_phone]
+                ]
+            };
+
+            
+
+            doc.moveDown().table(table2, {
+                prepareHeader: () => doc.font('Helvetica-Bold'),
+                prepareRow: (row, i) => doc.font('Helvetica').fontSize(12)
+            });
+
+            doc.pipe(res);
+            doc.end();
+        }
+
+        // res.send(result);
     });
 
-    const table1 = {
-        headers: ['Country', 'Conversion rate', 'Trend'],
-        rows: [
-            ['Switzerland', '12%', '+1.12%'],
-            ['France', '67%', '-0.98%'],
-            ['England', '33%', '+4.44%']
-        ]
-    };
 
-    doc.moveDown().table(table1, {
-        prepareHeader: () => doc.font('Helvetica-Bold'),
-        prepareRow: (row, i) => doc.font('Helvetica').fontSize(12)
-    });
-
-    doc.pipe(res);
-    doc.end();
 });
 
 module.exports = router;
